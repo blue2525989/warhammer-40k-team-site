d2e4566070862d62d448b4d02b320b5f36c90b38
bierbraj <bierbraj@wwt.com>
2025-09-09 07:53:24 -0500

    add dist

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/warhammer-40k-team-site/',
   plugins: [vue()],
   server: {
     port: 3000,
     open: true
+  },
+  define: {
+    global: 'globalThis',
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    }
   }
 })
